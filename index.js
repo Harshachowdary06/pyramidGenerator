18859118126855bffa3e8d30404f5fa76b42652b
Sai Sri Harsha Pemmasani <sriharshapemmasani06@gmail.com>
2025-02-16 21:08:10 -0500

    buldt square and rectangle

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function build(){
+let globalSymbol = "*";
+let character =""
 
-    const character = document.getElementById("inputBox").value;
-    const count = 8;
+function build(){
+    character = document.getElementById("inputBox").value;
+    if(character == ""){
+            character = globalSymbol;
+            console.log("hi " + character);
+    }
+    buildTriangle();
+}
+let count =8;
+function buildTriangle(){
+
+    
+    
     const rows = [];
 
     function padRow(rowNumber, rowCount) {
@@ -21,4 +33,52 @@ function build(){
 
     console.log(result1);
     document.getElementById("buildImage").innerHTML=result1;
+}
+
+// code for shape shift
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+function shapeShift(){
+    const shape = getRandomInt(1,5);
+    console.log(shape);
+    document.getElementById("buildImage").innerHTML="";
+    switch(shape){
+        case 1:
+            console.log("square");
+            buildSquare(1);
+            break;
+        case 2:
+            console.log("rectangle");
+            buildSquare(2);
+            break;
+        case 3:
+            console.log("circle");
+            break;
+        case 4:
+            console.log("diamond");
+            break;
+        case 5:
+            console.log("triangle");
+            buildTriangle();
+            break;
+    }
+}
+
+function buildSquare(num){
+    let rect = count;
+    if(num == 1){
+        rect =count/2;
+    }
+    let squarevertical = character.repeat(count);
+    let square = "";
+    for (let i =0;i<rect;i++){
+        square = square + squarevertical +"\n";
+    }
+    console.log(square);
+    document.getElementById("buildImage").innerHTML=square;
+
 }
